a526c9592a170d7748a14cec33855332485fc53b
Akileshy07 <yakilesh544@gmail.com>
2024-04-14 02:46:38 -0700

    setup gh-pages

--- smartcity/src/Components/Caraousel.jsx
+++ smartcity/src/Components/Caraousel.jsx
@@ -17,7 +17,7 @@ const Caraousel = () => {
         <>
             <div className="main col-md-12">
                 <div className="main-content">
-                    <h1>Smart city</h1>
+                    <h1>SMART CITY</h1>
                     <p>"Discover the future of urban living with our Smart City website.By simply entering your city name, we provide you with Comprehensive list of nearby amenities such as school,College,hospital and tourist attraction.</p>
                     <h3 className='mb-2'>Experience the convenience of smart city at your fingertips</h3>
                     <div className="search">
